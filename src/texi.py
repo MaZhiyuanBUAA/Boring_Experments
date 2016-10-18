60c8d62a3f5652b006ad4795ed238e7d3fba129c
MaZhiyuan <2265327245@qq.com>
2016-10-18 20:19:02 +0800

    fix some bugs

--- src/texi.py
+++ src/texi.py
@@ -2,69 +2,129 @@
 import copy
 import numpy as np
 import matplotlib.pyplot as plt
-np.random.seed(123)
-lx = 5000
-ly = 5000
-vstd = 1000000#distance variance
-vtstd = 50000000#time variance
-num_p = 1000#num of passengers
-mean = [lx,ly]
-cov = [[vstd,0],[0,vstd]]
-texis = np.random.multivariate_normal(mean,cov,num_p)#1000 cars
-x,y = texis.T
-#plt.plot(x, y, 'x'); plt.axis('equal'); plt.show()
-
-mean1 = [lx,ly,12*60*60]
-cov1 = [[vstd,0,0],[0,vstd,0],[0,0,vtstd]]
-passengers = np.random.multivariate_normal(mean1,cov1,num_p)#3000 passengers
-passengers = list(passengers)
-passengers.sort(key=lambda x:x[2])
+def generateTexisPassengers(seed,lx=5000,ly=5000,sigma1=4e3,sigma2=2.5*3600,num_orders=1000):
+    #texis:[(x,y)]
+    #passengers:[(x,y,t)]
+    np.random.seed(seed)
+    mean = [lx,ly]
+    cov = [[sigma1**2,0],[0,sigma1**2]]
+    texis = np.random.multivariate_normal(mean,cov,num_orders)#1000 cars
+    x,y = texis.T
+    #plt.plot(x, y, 'x'); plt.axis('equal'); plt.show()
+    
+    mean1 = [lx,ly,12*60*60]#midi
+    cov1 = [[sigma1**2,0,0],[0,sigma1**2,0],[0,0,sigma2**2]]
+    passengers = np.random.multivariate_normal(mean1,cov1,num_orders)#3000 passengers
+    passengers = list(passengers)
+    passengers.sort(key=lambda x:x[2])
+    return texis,passengers
 #print passengers[0:10]
 #x1,y1,_ = passengers
 #plt.plot(x1, y1, 'x'); plt.axis('equal'); plt.show()
-tmp_texi = copy.deepcopy(texis)
-distance = 0
-for ele in passengers:
-    ele = np.array(tmp_texi.shape[0]*[ele[0:2]])
-    tds = np.linalg.norm(ele-tmp_texi,axis=0)
-    ind = np.argmin(tds)
-    distance += tds[ind]
-    np.delete(tmp_texi,ind,axis=0)
-print distance
+def miniDist(texis,passengers):
+    tmp_texi = copy.deepcopy(texis)
+    distance = 0
+    for ele in passengers:
+        ele = np.array(tmp_texi.shape[0]*[ele[0:2]])
+        tds = np.linalg.norm(ele-tmp_texi,axis=0)
+        ind = np.argmin(tds)
+        distance += tds[ind]
+        np.delete(tmp_texi,ind,axis=0)
+    return distance
 
-tmp_texi = copy.deepcopy(texis)
-distance = 0
-for i,ele1 in enumerate(passengers):
-    ele1_v = np.array(tmp_texi.shape[0]*[ele1[0:2]])
-    tds1 = np.linalg.norm(ele1_v-tmp_texi,axis=0)
-    ind1 = np.argmin(tds1)
-    try:
-        ele2 = passengers[i+1]
-    except:
-        distance += tds1[ind1]
-        break
-    ele2_v = np.array(tmp_texi.shape[0]*[ele2[0:2]])
-    tds2 = np.linalg.norm(ele2_v-tmp_texi,axis=0)
-    ind2 = np.argmin(tds2)
-
-    if ind1 == ind2:
-        tds1_t = copy.deepcopy(tds1)
-        np.delete(tds1_t,ind1,axis=0)
-        ind1_2nd = np.argmin(tds1_t)
-        tds2_t = copy.deepcopy(tds2)
-        np.delete(tds2_t,ind2,axis=0)
-        ind2_2nd = np.argmin(tds2_t)
-        strategy1 = tds1[ind1] + tds2_t[ind2_2nd]
-        strategy2 = tds1_t[ind1_2nd] + tds2[ind2]
-        if strategy1 < strategy2:
+def bigramDist(texis,passengers):
+    tmp_texi = copy.deepcopy(texis)
+    distance = 0
+    for i,ele1 in enumerate(passengers):
+        ele1_v = np.array(tmp_texi.shape[0]*[ele1[0:2]])
+        tds1 = np.linalg.norm(ele1_v-tmp_texi,axis=1)
+        ind1 = np.argmin(tds1)
+        try:
+            ele2 = passengers[i+1]
+        except:
             distance += tds1[ind1]
-            np.delete(tmp_texi,ind1,axis=0)
-        else:
-            distance += tds1_t[ind1_2nd]
-            if ind1>ind1_2nd:
-                np.delete(tmp_texi,ind1_2nd,axis=0)
-            else:
-                np.delete(tmp_texi,ind1_2nd+1,axis=0)
-print distance
-
+            break
+        ele2_v = np.array(tmp_texi.shape[0]*[ele2[0:2]])
+        tds2 = np.linalg.norm(ele2_v-tmp_texi,axis=1)
+        ind2 = np.argmin(tds2)
     
+        if ind1 == ind2:
+            tds1_t = np.delete(tds1,ind1,axis=0)
+            ind1_2nd = np.argmin(tds1_t)
+            tds2_t = np.delete(tds2,ind2,axis=0)
+            ind2_2nd = np.argmin(tds2_t)
+            strategy1 = tds1[ind1] + tds2_t[ind2_2nd]
+            strategy2 = tds1_t[ind1_2nd] + tds2[ind2]
+            if strategy1 < strategy2:
+                distance += tds1[ind1]
+                tmp_texi = np.delete(tmp_texi,ind1,axis=0)
+            else:
+                distance += tds1_t[ind1_2nd]
+                if ind1>ind1_2nd:
+                    tmp_texi = np.delete(tmp_texi,ind1_2nd,axis=0)
+                else:
+                    tmp_texi = np.delete(tmp_texi,ind1_2nd+1,axis=0)
+        else:
+            distance += tds1[ind1]
+            tmp_texi = np.delete(tmp_texi,ind1,axis=0)
+    return distance
+
+def arg2min(a):
+    if not isinstance(a,np.ndarray):
+        return IOError
+    else:
+        ind1 = np.argmin(a)
+        a_tmp = np.delete(a,ind1,axis=0)
+        ind2 = np.argmin(a_tmp)
+        if ind1 > ind2:
+            ind2 += 1
+        return ind1,ind2
+def buildSet(texis,passengers):
+    data = []#(X,Y):((x1,y1,x2,y2,xp,yp,t),(0,1))
+    tmp_texi = copy.deepcopy(texis)
+    for i in range(len(passengers)-1):
+        ele1 = passengers[i]
+        ele1_v = np.array(tmp_texi.shape[0]*[ele1[0:2]])
+        tds1 = np.linalg.norm(ele1_v-tmp_texi,axis=1)
+        ind11,ind12 = arg2min(tds1)
+        #print ind11,ind12
+        ele2 = passengers[i+1]
+        ele2_v = np.array(tmp_texi.shape[0]*[ele2[0:2]])
+        tds2 = np.linalg.norm(ele2_v-tmp_texi,axis=1)
+        ind21,ind22 = arg2min(tds2)
+        if ind11 == ind21:
+            #compare
+            strategy1 = tds1[ind11] + tds2[ind22]
+            strategy2 = tds1[ind12] + tds2[ind21]
+            if strategy1 < strategy2:
+                data.append((np.hstack((tmp_texi[ind11],tmp_texi[ind12],ele1)),np.array([1.,0.])))
+                tmp_texi = np.delete(tmp_texi,ind11,axis=0)
+            else:
+                data.append((np.hstack((tmp_texi[ind11],tmp_texi[ind12],ele1)),np.array([0.,1.])))
+                tmp_texi = np.delete(tmp_texi,ind12,axis=0)
+        else:
+            data.append((np.hstack((tmp_texi[ind11],tmp_texi[ind12],ele1)),np.array([1.,0.])))
+            tmp_texi = np.delete(tmp_texi,ind11,axis=0)
+    return data
+#a test
+#texis,passengers = generateTexisPassengers(seed=1)
+#print miniDist(texis, passengers)
+#print bigramDist(texis, passengers)
+
+#10 days'data for training,1 day's for testing
+seedTrain = range(11)
+dataSet = []
+for ele in seedTrain:
+    print ele
+    texis,passengers = generateTexisPassengers(seed=ele)
+    dataSet += buildSet(texis, passengers)
+data_x,data_y = [],[]
+#data_x:11*1000x7
+#data_y:11*1000x2
+for ele in dataSet:
+    data_x.append(ele[0])
+    data_y.append(ele[1])
+data_x = np.array(data_x)
+data_y = np.array(data_y)
+data_x.tofile('../data/data_x.bin')
+data_y.tofile('../data/data_y.bin')
---
+++ src/trainModel.py
@@ -0,0 +1,111 @@
+'''
+A Multilayer Perceptron implementation example using TensorFlow library.
+This example is using the MNIST database of handwritten digits
+(http://yann.lecun.com/exdb/mnist/)
+
+Author: Aymeric Damien
+Project: https://github.com/aymericdamien/TensorFlow-Examples/
+'''
+
+# Import MINST data
+
+import tensorflow as tf
+import numpy as np
+#load data
+trainSize = 10*1000
+data_x = np.fromfile('../data/data_x.bin',dtype=np.float64)
+data_y = np.fromfile('../data/data_y.bin',dtype=np.float64)
+data_x = np.reshape(data_x,[-1,7])
+#data_x[:,0:6] = np.round(data_x[:,0:6]/10,0)
+#data_x[:,6] = np.round(data_x[:,6]/120,0)
+print data_x[0:10,:]
+#print data_x.shape
+data_y = np.reshape(data_y,[-1,2])
+#print data_y[0:10,:]
+trainX = data_x[0:trainSize,:]
+trainY = data_y[0:trainSize,:]
+testX = data_x[trainSize:,:]
+#print testX.shape
+testY = data_y[trainSize:,:]
+#print testY.shape
+#print testY
+# Parameters
+learning_rate = 0.001
+training_epochs = 20
+batch_size = 1000
+display_step = 1
+
+# Network Parameters
+n_hidden_1 = 8# 1st layer number of features
+n_hidden_2 = 8 # 2nd layer number of features
+n_input = 7 # MNIST data input (img shape: 28*28)
+n_classes = 2 # MNIST total classes (0-9 digits)
+
+# tf Graph input
+x = tf.placeholder("float", [None, n_input])
+y = tf.placeholder("float", [None, n_classes])
+
+
+# Create model
+def multilayer_perceptron(x, weights, biases):
+    # Hidden layer with Tanh activation
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.tanh(layer_1)
+    # Hidden layer with RELU activation
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
+    # Output layer with linear activation
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    return tf.nn.softmax(out_layer)
+
+# Store layers weight & bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+}
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+# Construct model
+pred = multilayer_perceptron(x, weights, biases)
+
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# Initializing the variables
+init = tf.initialize_all_variables()
+saver = tf.train.Saver()
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+
+    # Training cycle
+    for epoch in range(training_epochs):
+        avg_cost = 0.
+        total_batch = int(trainX.shape[0]/batch_size)
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_x, batch_y = trainX[i*batch_size:(i+1)*batch_size,:],trainY[i*batch_size:(i+1)*batch_size,:]
+            # Run optimization op (backprop) and cost op (to get loss value)
+            _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
+                                                          y: batch_y})
+            # Compute average loss
+            avg_cost += c / total_batch
+        # Display logs per epoch step
+        if epoch % display_step == 0:
+            print "Epoch:", '%04d' % (epoch+1), "cost=", \
+                "{:.9f}".format(avg_cost)
+    print "Optimization Finished!"
+    saver_path = saver.save(sess, "../model/mlp.m")
+    print "Model saved in file: ", saver_path
+
+    # Test model
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    # Calculate accuracy
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print "Accuracy:", accuracy.eval({x:testX,y:testY})
